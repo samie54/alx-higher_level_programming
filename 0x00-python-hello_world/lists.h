a5b25d4e5c446be9dfbf6f02414cbc5d5e6dd9b7
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-05 10:19:57 -0700

    lists.h

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,38 +1,25 @@
-#ifndef LISTS_H_
-#define LISTS_H_
-
-/*********************************************************
- * Project: 
- *
- *
- * Date: 05/06/2023
- *
- *
- * Author: Samuel Atiemo
- *    
- *
- **********************************************************/
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
-int check_cycle(listint_t *list);
-size_t print_listint(const listint_t *h);
-void free_listint(listint_t *head);
-listint_t *add_nodeint(listint_t **head, const int n);
-
-/*Struct commands*/
 
 /**
- * struct listint_s - singly linkedd list.
- * @n: integerr to be pointed
- * @next: pointer to next node
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Des: singgly linkedd listt structuree.
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
-#endif
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
+#endif /* LISTS_H */
