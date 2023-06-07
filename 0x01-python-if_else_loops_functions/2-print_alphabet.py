60c2455a46967eed16f97ac055671c74237bc0e6
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-07 07:54:04 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# This function prints the lowercase alphabet.
-def print_alphabet():
-    for char in range(ord('a'), ord('z') + 1):
-        print(chr(char), end="")
- 
- 
-if __name__ == "__main__":
-    print_alphabet()
+for b in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(b), end="")
