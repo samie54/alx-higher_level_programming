88881bc5336b1edf7ce7642c3899775d7c2711d7
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-07 08:05:21 -0700

    4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 def print_hex(start, end):
+  """Prints the hexadecimal representations of the numbers from start to end.
+ 
+  Args:
+    start: The starting number.
+    end: The ending number.
+ 
+  """
   for i in range(start, end + 1):
     print("{} = {}".format(i, hex(i)))
-
-print_hex(0, 99)
+ 
+if __name__ == "__main__":
+  print_hex(0, 99)
