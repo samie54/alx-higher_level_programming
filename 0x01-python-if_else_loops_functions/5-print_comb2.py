3ecccf0bf5236a73e0870f001329748994bc15f7
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-07 08:19:33 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for i in range(100):
-    print '{:0>2d}'.format(i)
+def print_numbers(start, end):
+  for i in range(start, end + 1):
+    print("{:0>2d}".format(i), end=", ")
+
+if __name__ == "__main__":
+  print_numbers(0, 100)
