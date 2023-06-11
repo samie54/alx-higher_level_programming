e1188528e88ed9cb598604a0472329ddbefeb777
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-10 17:29:22 -0700

    lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -12,17 +12,16 @@
  * Description: singly linkedd listt node structure.
  * for 0x03-python-data_structures project.
  */
-int is_palindrome(listint_t **head);
-
-listint_t *add_nodeint_end(listint_t **head, const int n);
-size_t print_listint(const listint_t *h);
-void free_listint(listint_t *head);
-
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int is_palindrome(listint_t **head);
 
 
 #endif /* LISTS_H_ */
