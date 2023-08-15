aa62d82ac5ca66678f43b6f5e7c197da179b55ef
samie54 <126026530+samie54@users.noreply.github.com>
2023-08-15 14:13:24 -0700

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
--- Lists all records of the table second_table.
--- Records are ordered by descending score.
+-- lists all records of the table second_table of
+--  the database hbtn_0c_0 in MySQL server.
 SELECT `score`, `name`
 FROM `second_table`
 ORDER BY `score` DESC;
