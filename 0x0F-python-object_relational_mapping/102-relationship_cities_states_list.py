15b37d44d85486d33307f20fa7c344dc1c826cac
samie54 <126026530+samie54@users.noreply.github.com>
2023-09-17 14:24:49 -0700

    Task 17

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+""" prints state object /w name passed as an argument, from database.
 """
 import sys
 from relationship_state import Base, State
