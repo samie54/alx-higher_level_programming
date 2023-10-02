5077075a67768592cfe1f8031ada9676824593e7
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-02 12:35:28 -0700

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script: accepts in a URL; sends a request to the URL and displays the value
+"""Python script: accepts in a URL; sends a request to the URL and displays the value.
 
 """
 import sys
