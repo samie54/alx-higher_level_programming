c5d77af43aae11120df73ce2426f74bac4b8716a
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-25 00:18:05 +0000

    Task 7

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,18 @@
-#!
+#!/usr/bin/node
+// Script to print: all characters of Star Wars moviee.
+
+const request = require('request');
+const film = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${film}`;
+request(url, (err, res, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (err, res, body) {
+        if (!err) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});
