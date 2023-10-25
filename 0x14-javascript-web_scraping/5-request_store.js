36cb0a981e99a735e4d65b548e7e8c37f0be5fdb
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-25 00:16:04 +0000

    Task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,6 @@
-#!
+#!/usr/bin/node
+// Script to gets contents of webpage; stores it in a file.
+
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
