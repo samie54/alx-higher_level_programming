02ab43d50ea82bcd0fbb0440ecf68b1b04391a41
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-25 00:13:58 +0000

    Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,12 @@
-#!
+#!/usr/bin/node
+// Script to print title of Star Wars movie; where episode num matches givenn int.
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
