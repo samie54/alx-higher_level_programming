a7d70950327124cfda9102906bde231f8e570459
samie54 <126026530+samie54@users.noreply.github.com>
2023-11-04 13:51:13 -0700

    Task 9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,7 @@
-$!
+$(() => {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', (data, textStatus) => {
+    if (textStatus === 'success') {
+      $('div#hello').text(data.hello);
+    }
+  });
+});
